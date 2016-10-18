483541bc53016a63167889ad79c83348624421fe
Catherine Nitta <cat@airbears2-10-142-59-248.airbears2.1918.berkeley.edu>
2016-10-17 19:42:44 -0700

    Update files

---
+++ pages/contact.js
@@ -0,0 +1,24 @@
+@import "Home"; 
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import { config } from 'config';
+import { Link } from 'react-router';
+import { prefixLink } from 'gatsby-helpers';
+
+export default class Contact extends Component {
+  render () {
+    return (
+      <div className="page page--contact">
+        <Helmet
+          title={config.siteTitle}
+        />
+        <h1>
+          Contact
+        </h1>
+        <a href="#"><img src='../img/fb.png' height="50" width="50" /></a>
+        <a href="#"><img src='../img/email.png' height="50" width="50" /></a>
+      </div>
+    );
+  }
+}
+
