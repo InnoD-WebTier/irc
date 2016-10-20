739dd715470055f2e27b0faad1a07587f6664d92
Peter Qiu <peterqiu1997@gmail.com>
2016-10-19 20:02:47 -0700

    more navbar

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -6,7 +6,10 @@ import classNames from 'classnames';
 
 require('css/_index.scss');
 
+const navbar_height = 54;
+
 export default class Template extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
@@ -26,48 +29,50 @@ export default class Template extends Component {
   render () {
     return (
       <div>
-        <Headroom>
+        <Headroom wrapperStyle={{
+            maxHeight: navbar_height
+          }}>
           <div className={classNames("navbar", {
               "navbar--open": this.state.open
             })}>
             <div
-                className={classNames("nav__hamburger", {
-                  "nav__hamburger--active": this.state.open
+                className={classNames("navbar__hamburger", {
+                  "navbar__hamburger--active": this.state.open
                 })}
                 onClick={this._handleHamburger}
               >
               <div className="hamburger__bar bar--1"></div>
               <div className="hamburger__bar bar--2"></div>
             </div>
-            <div className="navbar__link--wrapper">
+            <div className="navbar__link--wrapper" onClick={this._handleHamburger}>
               <Link
                 to={prefixLink('/')} className="navbar__link"
               >
                 Home
               </Link>
             </div>
-            <div className="navbar__link--wrapper">
+            <div className="navbar__link--wrapper" onClick={this._handleHamburger}>
               <Link
                 to={prefixLink('/about/')} className="navbar__link"
               >
                 About
               </Link>
             </div>
-            <div className="navbar__link--wrapper">
+            <div className="navbar__link--wrapper" onClick={this._handleHamburger}>
               <Link
                 to={prefixLink('/events/')} className="navbar__link"
               >
                 Events
               </Link>
             </div>
-            <div className="navbar__link--wrapper">
+            <div className="navbar__link--wrapper" onClick={this._handleHamburger}>
               <Link
                 to={prefixLink('/review/')} className="navbar__link"
               >
                 Review
               </Link>
             </div>
-            <div className="navbar__link--wrapper">
+            <div className="navbar__link--wrapper" onClick={this._handleHamburger}>
               <Link
                 to={prefixLink('/contact/')} className="navbar__link"
               >
