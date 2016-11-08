118992d5cb06a596c867b05360546c960f8fd1a0
ni-tta <ni-tta@users.noreply.github.com>
2016-11-07 20:33:12 -0800

    black circular icons and center-aligned list of organizations (#14)
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files
    
    * create contact.scss
    
    * 	modified:   css/pages/contact.scss
    
    * include fontawesome
    
    * add email and fb icons
    
    * add email and fb icons
    
    * remove DS_Store
    
    * fix formatting in contact js/scss
    
    * .DS_Store removed
    
    * Increase spacing between elements of contact page
    
    * apply _base.scss styles to contact.js
    
    * center align contact info
    
    * center aligned contact info
    
    * Add black circle background to social media icons
    
    * re-add necessary style in _base.scss and add wrapper with margins
    
    * adjust margins
    
    * adjust margins

--- pages/contact.js
+++ pages/contact.js
@@ -12,28 +12,118 @@ export default class Contact extends Component {
         <Helmet
           title={config.siteTitle}
         />
-        <div className="contact--center">
-
-          <div className="contact--title">
+      
+        <div className="page__title">
             Contact
-          </div>
+        </div>
 
-          <a href="#" target="_blank">
-            <FontAwesome
-              className="media--icon fb"
-              name="facebook"
-            /> 
-          </a>
+        <div className="page__content">
+          <div className="contact__body">
+            <div className="contact__name">International Relations Council </div>
+            <div className="contact__icons">
+              <a href="#" target="_blank">
+                <FontAwesome
+                  className="media__icon circle fb"
+                  name="facebook"
+                /> 
+              </a>
 
-          <a href="#" target="_blank">
-            <FontAwesome
-              className="media--icon mail"
-              name="envelope"
-            /> 
-          </a> 
+              <a href="mailto:ircofberkeley@gmail.com" target="_blank">
+                <FontAwesome
+                  className="media__icon mail"
+                  name="envelope"
+                /> 
+              </a>  
+              </div>
 
+              <div className="contact__name">Amnesty International</div>
+              <div className="contact__icons">
+               <a href="#" target="_blank">
+                <FontAwesome
+                  className="media__icon fb"
+                  name="facebook"
+                /> 
+                </a>
+
+                <a href="mailto:ucbamnesty@gmail.com" target="_blank">
+                  <FontAwesome
+                    className="media__icon mail"
+                    name="envelope"
+                  /> 
+                </a> 
+              </div>
+       
+
+              <div className="contact__name">Berkeley Model United Nations</div>
+              <div className="contact__icons">
+                <a href="#" target="_blank">
+                  <FontAwesome
+                    className="media__icon fb"
+                    name="facebook"
+                  /> 
+                </a>
+
+                <a href="mailto:info@bmun.org" target="_blank">
+                  <FontAwesome
+                    className="media__icon mail"
+                    name="envelope"
+                  /> 
+                </a>
+              </div> 
+
+              <div className="contact__name">Delta Phi Epsilon</div> 
+              <div className="contact__icons"> 
+                <a href="#" target="_blank">
+                  <FontAwesome
+                    className="media__icon fb"
+                    name="facebook"
+                  /> 
+                </a>
+
+                <a href="mailto:berkeleydpe@gmail.com" target="_blank">
+                  <FontAwesome
+                    className="media__icon mail"
+                    name="envelope"
+                  /> 
+                </a> 
+              </div>
+
+              <div className="contact__name">Sigma Iota Rho</div>
+              <div className="contact__icons">
+                <a href="#" target="_blank">
+                  <FontAwesome
+                    className="media__icon fb"
+                    name="facebook"
+                  /> 
+                </a>
+
+                <a href="mailto:sigmaiotarho.berkeley@gmail.com" target="_blank">
+                  <FontAwesome
+                    className="media__icon mail"
+                    name="envelope"
+                  /> 
+                </a>
+              </div>
+
+              <div className="contact__name">UC Berkeley Model United Nations</div>
+              <div className="contact__icons">
+                <a href="#" target="_blank">
+                  <FontAwesome
+                    className="media__icon fb"
+                    name="facebook"
+                  /> 
+                </a>
+
+                <a href="mailto:president@ucbmun.org" target="_blank">
+                  <FontAwesome
+                    className="media__icon mail"
+                    name="envelope"
+                  /> 
+                </a>
+              </div>
+            </div> 
+          </div> 
         </div>   
-      </div>
     );
   }
 }
