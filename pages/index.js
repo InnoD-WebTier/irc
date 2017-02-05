06f6ac007343a51f23d205972f56616cb6cb2942
Julia Sun <julia.sun@berkeley.edu>
2017-02-05 10:25:54 -0800

    add background picture

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,15 @@ import { prefixLink } from 'gatsby-helpers';
 export default class Home extends Component {
   render () {
     return (
-      <div className="page page--home">
+      <div 
+        className="page page--home"
+//         style={{
+//           backgroundImage: `url(${prefixLink('/img/logo.jpg')})`
+//         }}
+      >
+        <div className="img--home">
+          <img src="/img/logo.jpg"/>
+        </div>
         <Helmet
           title={config.siteTitle}
         />
